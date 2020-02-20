1b107a82531726c2ef5900f91a79f8532008e131
Jared Nelson <jaredlnelson1@gmail.com>
2020-02-19 21:36:06 -0800

    Update as1.cpp
    Print list of values to file, moved boxmuller into seperate cpp to make less clutter.  Still trying to figure out what exactly Prof wants for bayesclassifer....

--- as1.cpp
+++ as1.cpp
@@ -1,54 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
+#include <math.h>
 using namespace std;
 
-
-double ranf(double m) {
-	return (m * rand()) / (double)RAND_MAX;
-}
-
-float generateSamples(int mu, int sigma) {
-
-	float x1, x2, w, y1;
-	static float y2;
-	static int use_last = 0;
-
-	if (use_last) {
-		y1 = y2;
-		use_last = 0;
-	}
-
-	else {
-		do {
-			x1 = 2.0 * ranf(1) - 1.0;
-			x2 = 2.0 * ranf(1) - 1.0;
-			w = x1 * x1 + x2 * x2;
-		} while (w >= 1.0);
-
-		w = sqrt((-2.0 * log(w)) / w);
-		y1 = x1 * w;
-		y2 = x2 * w;
-		use_last = 1;
-	}
-
-	return (mu + y1 * sigma);
-
-}
-
-
-
-
+double ranf(double m);
+float generateSamples(int mu, int sigma);
+void printToFile(vector<float> &valX, vector<float> &valY, string outputfile);
+void bayesClassifier(vector<float> &valX, vector<float> &valY, float muOne, float muTwo, float varianceOne, float varianceTwo);
 int main()
 {
-
 	vector<float> x1, y1, x2, y2;
 
 	for (int i = 0; i < 50000; ++i) {
 		x1.push_back(generateSamples(1, 1));
-		x2.push_back(generateSamples(1, 1));
-		y1.push_back(generateSamples(4, 1));
+		y1.push_back(generateSamples(1, 1));
+		x2.push_back(generateSamples(4, 1));
 		y2.push_back(generateSamples(4, 1));
+        
 	}
+    printToFile(x1, y1, "Sample1.txt");
+	printToFile(x2, y2, "Sample2.txt");
 
 }
 
+
+
+
+
+void printToFile(vector<float> &valX, vector<float> &valY, string outputfile){
+    ofstream outfile(outputfile);
+        outfile << " X VALUE    Y VALUE" << endl;
+    for(int i = 0; i < 50000; ++i){
+        outfile << "(" << valX[i] << ", " << valY[i] << ")" << endl;
+    }
+}
+
+void bayesClassifier(vector<float> &valX, vector<float> &valY, float muOne, float muTwo, float varianceOne, float varianceTwo){
+    
+}
